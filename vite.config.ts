139493d97787fdd94fd5a8fb9e26b9506f6ae32d
ArifLaode <muhamadariflaode99@gmail.com>
2025-09-21 17:45:15 +0800

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import sitemapPlugin from 'vite-plugin-sitemap'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    sitemapPlugin({
-      hostname: 'https://ulunggolaka.icu',
-      outDir: './dist',
-    })
+    Sitemap({ hostname: 'ulunggolaka.icu',
+      generateRobotsTxt: true,
+      externalSitemaps: ['https://ulunggolaka.icu/sitemap.xml'],
+      changefreq: 'weekly',
+      priority: 0.8,
+      dynamicRoutes: [
+        "/",          // Home
+        "/profil",    // Profil
+        "/layanan",   // Layanan
+        "/berita",    // List berita
+        "/kontak"     // Kontak
+      ]
+     }),
   ],
   base: '/',
 })
